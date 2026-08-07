33669817c0759293d23dff0086cb4c6eabe61585
Natmanee L. <NatmaneeL@arv.co.th>
2026-08-08 02:02:44 +0700

    add testing file

---
+++ 01-syntax.js
@@ -0,0 +1,14 @@
+// valiable name can be Hyphens (last-name), underscore (first_name), upper camel(FirstName), lower camel(fisrtName)
+//JavaScript is Case Sensitive
+// The variables lastName and lastname, are different variables:
+let firstName = 'Natmanee'
+let lastName = 'Losawatkul'
+
+// JavaScript Operators
+// using operators (=) assign values to variables:
+let x = 5
+let y = 6
+let sum = x * y
+console.log(sum)
+console.log(firstName +" "+ lastName)
+
